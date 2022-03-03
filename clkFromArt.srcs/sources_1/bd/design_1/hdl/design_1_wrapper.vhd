02cfadfcfd333226ac676ff43a3f296931896891
Alx <aabcad@gmail.com>
2022-03-03 17:50:13 +0300

    'Merged' with clkb 2

--- clkFromArt.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ clkFromArt.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
---Date        : Tue Mar  1 16:47:45 2022
+--Date        : Thu Mar  3 17:17:51 2022
 --Host        : alx-laptop running 64-bit Ubuntu 18.04.5 LTS
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
